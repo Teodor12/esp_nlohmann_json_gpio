c60ef055e8e2f8eb7d7ff5fd13d0751817325166
Teodor <fodor.teo@gmail.com>
2024-05-23 22:16:05 +0000

    use json_validator for toogling the requested gpio pin

--- components/gpio_handler/include/GpioHandler.hpp
+++ components/gpio_handler/include/GpioHandler.hpp
@@ -2,18 +2,22 @@
 
 #include "json.hpp"
 #include "esp_system.h"
-
+#include <iostream>
 
 struct GpioRequest {
 
-    NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(GpioRequest, gpio_num, state);
+public:
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(GpioRequest, gpio_num, state)
 
-    unsigned gpio_num;
-    bool state;
+    int gpio_num;
+    int state;
+
 };
+
+GpioRequest create_gpio_request(const std::string& json_request_str);
 
 esp_err_t gpio_init(uint8_t gpio_pin_1, uint8_t gpio_pin_2, uint8_t gpio_pin_3);
 
-esp_err_t update_gpio_level(uint8_t gpio_pin_number, bool gpio_state);
+esp_err_t update_gpio_levels(const GpioRequest& gpio_state);
 
-esp_err_t update_gpio_level(const GpioRequest& gpio_state);
+

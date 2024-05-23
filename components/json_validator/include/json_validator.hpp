13b05816b9edd9d302a91ecaf534ed82384df382
Teodor <fodor.teo@gmail.com>
2024-05-23 22:14:29 +0000

    implement json_validator functionality

--- components/json_validator/include/json_validator.hpp
+++ components/json_validator/include/json_validator.hpp
@@ -6,8 +6,4 @@
 
 using json = nlohmann::json;
 
-json json_handler_create_json(uint8_t gpio_num, bool gpio_state);
-
-json json_handler_parse_str(const std::string& json_string);
-
-std::string json_handler_serialize_json(const json& json_instance);
+esp_err_t parse_json_str(const std::string& json_str, json& json_arg);
--- components/json_validator/json_validator.cpp
+++ components/json_validator/json_validator.cpp
@@ -1,19 +1,31 @@
 #include "json_validator.hpp"
+#include "esp_log.h"
 
-json json_handler_create_json(uint8_t gpio_num, bool gpio_state){
-    auto ret = json::object();
-    ret["gpio_num"] = gpio_num;
-    ret["gpio_state"] = gpio_state;
-    return ret;
+#define JSON_VALIDATOR_LOG_TAG "json_validator"
+
+static esp_err_t validate_json_str(const std::string& json_str)
+{
+    if(!json::accept(json_str)) {
+        ESP_LOGE(JSON_VALIDATOR_LOG_TAG, "invalid json format");
+        return ESP_ERR_INVALID_ARG;
+    }
+    return ESP_OK;
 }
 
-json json_handler_parse_str(const std::string &json_string){
-    json json_instance;
-    json_instance = json::parse(json_string);
-    return json_instance;
-}
+esp_err_t parse_json_str(const std::string& json_str, json& json_arg)
+{
+    esp_err_t ret = validate_json_str(json_str);
+    if (ret != ESP_OK) {
+        return ret;
+    }
 
-std::string json_handler_serialize_json(const json &json_instance){
-    auto ret = json_instance.dump();
-    return ret;
+    try {
+        json_arg = json::parse(json_str);
+        return ESP_OK;
+    }
+    catch (const json::parse_error& parse_error) {
+        ESP_LOGW(JSON_VALIDATOR_LOG_TAG, "unable to parse json_str");
+        json_arg = json({});
+        return ESP_FAIL;
+    }
 }
